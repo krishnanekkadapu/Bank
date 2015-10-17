585f2e4ba0404dd20e308d72875b2fc8808f72e1
krishnanekkadapu <nekkadapu_8692@yahoo.com>
2015-10-17 01:32:37 -0500

    Transaction

--- src/main/java/com/bank/action/Transaction.java
+++ src/main/java/com/bank/action/Transaction.java
@@ -3,7 +3,14 @@ package com.bank.action;
 public class Transaction {
 
 	private String accountbalance;
+	private String balance;
 	
+	public String getBalance() {
+		return balance;
+	}
+	public void setBalance(String balance) {
+		this.balance = balance;
+	}
 	public Transaction()
 	{
 		
