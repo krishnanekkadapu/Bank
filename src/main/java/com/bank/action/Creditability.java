44c70ec2ff9fc0b179abd31dab9952f535e57457
krishnanekkadapu <nekkadapu_8692@yahoo.com>
2015-10-17 01:41:02 -0500

    Creditability
    added creditability class

---
+++ src/main/java/com/bank/action/Creditability.java
@@ -0,0 +1,14 @@
+package com.bank.action;
+
+public class Creditability {
+	private String creditIssued;
+
+	public String getCreditIssued() {
+		return creditIssued;
+	}
+
+	public void setCreditIssued(String creditIssued) {
+		this.creditIssued = creditIssued;
+	}
+
+}
